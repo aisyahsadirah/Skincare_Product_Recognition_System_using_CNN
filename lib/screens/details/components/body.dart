46f308e3b3fa3a86d640fc530f8c4b943f58431a
aisyahsadirah <76616107+aisyahsadirah@users.noreply.github.com>
2021-02-24 11:34:15 +0800

    Create body.dart

---
+++ lib/screens/details/components/body.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+//import 'package:holaskin/constants.dart';
+
+//import 'image_and_icons.dart';
+//import 'title_and_price.dart';
+
+class Body extends StatelessWidget {
+  const Body({
+    Key key,
+    @required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
